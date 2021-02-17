705112ea7e382b26bb503e971ece796b0d105581
YiBook <1275432232@qq.com>
2021-02-17 21:42:46 +0800

    feat: article pages with db

--- handler/article.go
+++ handler/article.go
@@ -5,13 +5,20 @@ import (
 	"strconv"
 
 	"github.com/c-major/blog/common"
-	"github.com/c-major/blog/model"
+	"github.com/c-major/blog/db"
 	"github.com/gin-gonic/gin"
 )
 
 // ShowIndexPage .
 func ShowIndexPage(c *gin.Context) {
-	articleList := model.GetAllArticles()
+	common.TextLog.WithContext(c)
+
+	articleList, err := db.GetAllArticles()
+	if err != nil {
+		common.TextLog.Error("[ShowIndexPage] failed to get all articles from db")
+		c.AbortWithError(http.StatusNotFound, err)
+		return
+	}
 
 	common.Render(c,
 		gin.H{
@@ -23,14 +30,31 @@ func ShowIndexPage(c *gin.Context) {
 
 // GetArticle .
 func GetArticle(c *gin.Context) {
+	common.TextLog.WithContext(c)
+
 	articleID, err := strconv.ParseUint(c.Param("article_id"), 10, 64)
 	if err != nil {
+		common.TextLog.Error("[GetArticle] failed to parse url param")
 		c.AbortWithStatus(http.StatusNotFound)
+		return
 	}
 
-	article, err := model.GetArticleByID(articleID)
+	article, err := db.GetArticleByID(articleID)
 	if err != nil {
+		common.TextLog.Error("[GetArticle] failed to get article from db")
 		c.AbortWithError(http.StatusNotFound, err)
+		return
+	}
+
+	if article == nil {
+		common.Render(
+			c,
+			gin.H{
+				"title": "article not found",
+			},
+			"article.html",
+		)
+		return
 	}
 
 	common.Render(
--- handler/article_test.go
+++ handler/article_test.go
@@ -4,11 +4,34 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"strings"
 	"testing"
 
+	"github.com/c-major/blog/caller"
+	"github.com/c-major/blog/common"
 	"github.com/c-major/blog/util"
 )
+
+func init() {
+	common.InitLog()
+
+	rootDir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	os.Setenv("IS_TEST_ENV", "1")
+	config, err := common.GetConfig(rootDir, "../", "conf")
+	if err != nil {
+		common.TextLog.Error("failed to initialize config")
+	}
+
+	err = caller.InitCaller(config)
+	if err != nil {
+		common.TextLog.Error("failed to initialize caller")
+	}
+}
 
 // Test that a GET request to the home page returns the home page with
 // the HTTP code 200 for an unauthenticated user
@@ -48,7 +71,7 @@ func TestGetArticleUnauthenticated(t *testing.T) {
 		// You can carry out a lot more detailed tests using libraries that can
 		// parse and process HTML pages
 		b, err := ioutil.ReadAll(rr.Body)
-		pageOK := (err == nil && strings.Index(string(b), "<title>Article 1</title>") > 0)
+		pageOK := (err == nil && strings.Index(string(b), "<title>test title 1</title>") > 0)
 
 		return statusOK && pageOK
 	})
